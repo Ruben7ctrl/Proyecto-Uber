4791966293a764fc8530d4187e5f1c851cb24051
Ruben Cabello <rubencabellomiguel@gmail.com>
2025-07-23 21:02:07 +0000

    aliminados

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 # from models import Person
 
@@ -30,6 +31,7 @@ if db_url is not None:
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
